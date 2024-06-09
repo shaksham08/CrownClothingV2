1078c2266710a7a75e9cbcb74cc0f8c9eb95d81e
shaksham08 <shakshamsinha@outlook.com>
2024-06-09 12:11:25 +0530

    dev:added changes

---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,15 @@
+const SignIn = () => {
+  //   const logGoogleUser = async () => {
+  //     const response = await signInWithGooglePopup();
+  //     createUserProfileDocument(response);
+  //   };
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button>Sign in with Google Popup</button>
+    </div>
+  );
+};
+
+export default SignIn;
